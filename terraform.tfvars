2fc805f6a11dfd66af0078f1de931eeffdf02b1d
Rodrigo Figueira <rodrigoservice@gmail.com>
2025-01-14 18:12:05 -0300

    Inclusão do recurso da api e variáveis terraform

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+api_name = "crud_serverless"
+default_description = "Criado por Terraform"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "api_name" {
+  description = "Nome da Api"
+  type        = string
+}
+
+variable "default_description" {
+  description = "Descrição padrão"
+  type        = string
+}
