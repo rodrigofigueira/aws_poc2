9bdcb595d83f9181d111e39d215110d739dfbaac
Rodrigo Figueira <rodrigoservice@gmail.com>
2025-01-15 11:33:41 -0300

    separação api gateway em outro arquivo

--- main.tf
+++ main.tf
@@ -8,11 +8,3 @@ terraform {
 
   required_version = ">= 1.10.4"
 }
-
-resource "aws_api_gateway_rest_api" "api" {
-  name        = var.api_name
-  description = var.default_description
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
